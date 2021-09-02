c0608cdaa98533fee18f58570d4240eec9337159
Artur S <arczi11108@gmail.com>
2021-09-02 11:36:18 +0200

    Refactoring

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ import random
 import string
 
 
-def removeEverySecondLetter(name):
-    newName = ''
-    for i in range(0, len(name), 2):
-        newName += name[i]
-
-    return newName
-
-
 with open('names.txt', 'r') as rf:
     with open('new_names.txt', 'w') as wf:
         for line in rf:
@@ -18,5 +10,5 @@ with open('names.txt', 'r') as rf:
             wf.write(stripLine + ";" + stripLine.replace(random.choice(stripLine), '', 1))
             wf.write("\n" + stripLine + ';' + random.choice(string.ascii_letters).upper() + stripLine)
             wf.write("\n" + stripLine + ';' + stripLine[::-1])
-            wf.write("\n" + stripLine + ';' + removeEverySecondLetter(stripLine) + "\n")
+            wf.write("\n" + stripLine + ';' + stripLine[::2] + "\n")
 
